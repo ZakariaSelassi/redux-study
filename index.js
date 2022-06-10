79616be10dd2e67079f05018f666e1b32bca8690
zakaria selassi <zakariaselassi@gmail.com>
2022-06-10 12:27:18 +0200

    multipe reducer

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 const redux = require('redux')
-
-// redux createstore
+const bindActionCreators = redux.bindActionCreators
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
  // Exercice 1 :  Restocking Cakes
 
  // Everyday , a vendor comes to the shop to restock the shelves with cakes.
@@ -11,6 +11,10 @@ const createStore = redux.createStore
 // Step 1 : Define action
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED'
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED'
 
 const orderCake = () => {
     return {
@@ -19,7 +23,6 @@ const orderCake = () => {
     }
     
 }
-const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
 
 const restockCake = (qty = 1) => {
     return {
@@ -28,16 +31,37 @@ const restockCake = (qty = 1) => {
     }
 }
 
-// Step 2 : set initial State
-// define initialState 
-const initialState = {
-    numOfCakes:10,
+const orderIceCream = (qty = 1) => {
+    return {
+        type: ICECREAM_ORDERED,
+        payload: qty
+    }
 
 }
+const restockIceCream = (qty = 1) => {
+    return {
+        type: ICECREAM_RESTOCKED,
+        payload: qty
+    }
+}
 
+// Step 2 : set initial State
+// define initialState 
+/* const initialState = {
+    numOfCakes:10,
+    numOfIceCreams:20,
+} */
+
+const initialCakeState = {
+    numOfCakes:10,
+}
+
+const initialIceCreamState = {
+    numOfIceCreams:20,
+}
  // reducer accepts a state as an argument and return the next state of an application
- const reducer = (  state = initialState, action) => {
-     
+ const cakeReducer = (  state = initialCakeState, action) => {
+    
     switch(action.type){
         case CAKE_ORDERED :
                 // return new state of application as an object  
@@ -50,22 +74,52 @@ const initialState = {
                     ...state,
                     numOfCakes: state.numOfCakes + action.payload
                 }
+    
         default : 
                 return state
     }
  }
 
+ const iceCreamReducer = (  state = initialIceCreamState, action) => {
+    switch(action.type){
+        case ICECREAM_ORDERED :
+        // return new state of application as an object  
+                return {
+                    ...state, // first make a copy an then make an update 
+                    numOfIceCreams: state.numOfIceCreams - 1
+                }
+        case ICECREAM_RESTOCKED : 
+                return {
+                    ...state,
+                    numOfIceCreams: state.numOfIceCreams + action.payload
+                }
+        default : 
+                return state
+    }
+ }
 
+// using combineReducer allow to put multipe reducer inside create store , default createstore allow only single reducer passing in
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
 // hold application state
- const store = createStore(reducer) // creating store method that accepts reducer (initialState)
+ const store = createStore(rootReducer) // creating store method that accepts reducer (initialState)
  console.log('init state  ', store.getState())
 
  // Listener that check each time the state of the application changes
  const unsubscribe = store.subscribe(() => console.log("update state :" , store.getState()))
 
- store.dispatch(orderCake())
- store.dispatch(orderCake())
- store.dispatch(orderCake())
- store.dispatch(restockCake(4))
+ const actions = bindActionCreators({
+     orderCake,restockCake,
+    orderIceCream,restockIceCream
+ },store.dispatch)
+
+ actions.orderCake()
+ actions.orderCake()
+ actions.orderCake()
+ actions.orderIceCream()
+ actions.orderIceCream()
+ actions.restockIceCream(2)
  unsubscribe();
 
